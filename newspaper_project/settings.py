496e6c2872729688a8e52d1f3b2a913c6696daf1
Vsirotkin <vsirotkin15@gmail.com>
2022-05-31 17:37:04 +0300

    created login, signup and logout

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'newspaper_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIRS, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
