fa0c09b22396fe219eb7a559d364fa57b8f95243
vik <vsirotkin15@gmail.com>
2022-06-11 11:39:01 +0300

    created url-view-template for articles

--- newspaper_project/urls.py
+++ newspaper_project/urls.py
@@ -22,7 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
-    path('', include('pages.urls')), # home page
-    
-    # path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('articles/', include('articles.urls')), # new
+    path('', include('pages.urls')),
 ]
