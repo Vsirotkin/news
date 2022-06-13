4504e7ce4ba687571cccde49ea64b07300a17ac5
Viktor <vsirotkin15@gmail.com>
2022-06-13 13:00:39 +0300

    func to create new article

--- newspaper_project/urls.py
+++ newspaper_project/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
-    path('articles/', include('articles.urls')), # new
+    path('articles/', include('articles.urls')),
     path('', include('pages.urls')),
 ]
