406d118caeeb23ab5afc31ff10c78f63e39719f5
Vsirotkin <vsirotkin15@gmail.com>
2022-06-17 17:06:31 +0300

    add func to compose a new post from a user

--- articles/models.py
+++ articles/models.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls import reverse
+from django.views.generic.edit import CreateView
 
 
 # Create your models here.
